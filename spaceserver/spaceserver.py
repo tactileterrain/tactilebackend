84b4503489692df416ddf5bca7051328682a101c
Jordan Medlock <jordan@catalysis.com>
2020-10-03 16:20:52 -0700

    Added simple nasa API support

--- spaceserver/spaceserver.py
+++ spaceserver/spaceserver.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
 import math
 import random
+import requests
+from PIL import Image
+import tempfile
+import io
+import numpy as np
 
 app = Flask(__name__)
 
@@ -33,6 +38,7 @@ def get_layer(mid, lid):
 
 @app.route('/map/<mid>/pi')
 def get_pi_image(mid):
+    get_image()
     return None # pi_image
 
 @app.route('/map/<mid>/stl')
@@ -42,15 +48,33 @@ def get_map_stl(mid):
 @app.route('/map/<mid>/data')
 def get_map_data(mid):
     return jsonify({
-        'pixel_data': [
-            [
-                [random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)]
-                for _ in range(32)
-            ]
-            for _ in range(32)
-        ]
+        'pixel_data': get_image()
     })
 
+
+def get_image():
+    
+    buffer = tempfile.SpooledTemporaryFile(max_size=1e9)
+    r = requests.get('https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/MODIS_Terra_Aerosol/default/2014-04-09/GoogleMapsCompatible_Level6/1/1/1.png')
+    if r.status_code == 200:
+        downloaded = 0
+        filesize = int(r.headers['content-length'])
+        print('download started')
+        for chunk in r.iter_content():
+            downloaded += len(chunk)
+            buffer.write(chunk)
+        print('download done')
+        buffer.seek(0)
+        i = Image.open(io.BytesIO(buffer.read()))
+        print(i.mode)
+        print(i.size)
+        pic = np.array(i)
+        print(pic.size)
+    return [
+        [
+            [int(pic[i, j]), int(pic[i, j]), int(pic[i, j])] for i in range(32)
+        ] for j in range(32)
+    ]
 
 @app.route('/layers')
 def get_layer_list():
